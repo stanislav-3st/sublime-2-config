aa2cd0b92a2ce380fbf3fcf9c8d3467cfc32c5f2
stanislav-3st <mueller@3st-digital.de>
2013-03-14 10:03:58 +0100

    modules

--- Packages/drush_cc/drush_cc.py
+++
@@ -1,7 +0,0 @@
-import sublime
-import sublime_plugin
-from subprocess import call
-
-class DrushccCommand(sublime_plugin.WindowCommand):
-    def run(self):      
-        call(["/Users/st/bin/drush -r /Users/st/Sites/www/versatel_de cc css+js"], shell=True)
---
+++ Packages/objc .strings syntax language/README.md
@@ -0,0 +1,3 @@
+This is a Syntax language file for sublime, it defines the syntax of the .string files for iOS/OSX. 
+
+More information in http://bakingcode.com or @PaNaVTEC on twitter
